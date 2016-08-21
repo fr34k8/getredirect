e01a6a1622e70af7270e2e38e4d33d276808254c
Sebastian Broekhoven <s.broekhoven@gmail.com>
2016-08-21 20:55:18 +0200

    First version and function

--- README.md
+++ README.md
@@ -1,2 +1,83 @@
 # getredirect
 Get redirect of URL, programmed in Go.
+
+# Example usage
+
+```go
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/binaryfigments/getredirect"
+)
+
+func main() {
+	testsite := "https://www.binaryfigments.com"
+	header := getredirect.From(testsite)
+	json, err := json.MarshalIndent(header, "", "   ")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(string(json))
+}
+```
+
+# Example response
+
+```json
+{
+   "Question": {
+      "JobURL": "https://www.binaryfigments.com",
+      "JobStatus": "OK",
+      "JobMessage": "Job done!",
+      "JobTime": "2016-08-21T20:26:55.710800393+02:00"
+   },
+   "Answer": {
+      "HTTPStatus": 301,
+      "HTTPStatusText": "301 Moved Permanently",
+      "HTTPRedirect": "https://binaryfigments.com",
+      "HTTPProto": "HTTP/2.0",
+      "IPAddress": [
+         "2a01:448:1003::130",
+         "213.249.93.130"
+      ]
+   }
+}
+```
+
+# Dependencies
+
+ * Go 1.6.x tested https://golang.org
+ * httprouter https://github.com/julienschmidt/httprouter
+ * govalidator https://github.com/asaskevich/govalidator
+
+```
+go get github.com/julienschmidt/httprouter
+go get github.com/asaskevich/govalidator
+```
+
+# The MIT License (MIT)
+
+Copyright (c) 2016 Sebastian Broekhoven
+~~~
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+~~~
---
+++ main.go
@@ -0,0 +1,111 @@
+package getredirect
+
+import (
+	"log"
+	"net"
+	"net/http"
+	"net/url"
+	"strings"
+	"time"
+
+	"github.com/asaskevich/govalidator"
+)
+
+// Question struct
+type Question struct {
+	JobURL     string    `json:"JobURL"`
+	JobStatus  string    `json:"JobStatus"`
+	JobMessage string    `json:"JobMessage"`
+	JobTime    time.Time `json:"JobTime"`
+}
+
+// Answer struct
+type Answer struct {
+	HTTPStatus     int      `json:"HTTPStatus"`
+	HTTPStatusText string   `json:"HTTPStatusText"`
+	HTTPRedirect   string   `json:"HTTPRedirect,omitempty"`
+	HTTPProto      string   `json:"HTTPProto"`
+	IPAddress      []string `json:"IPAddress,omitempty"`
+	Connection     string   `json:"Connection,omitempty"`
+}
+
+// ReturnMessage for retunring
+type ReturnMessage struct {
+	Question Question `json:"Question"`
+	Answer   Answer   `json:"Answer"`
+}
+
+// From function
+func From(testurl string) *ReturnMessage {
+	h := new(ReturnMessage)
+	h.Question.JobTime = time.Now()
+	h.Question.JobURL = testurl
+
+	url, err := url.Parse(testurl)
+	if err != nil {
+		panic(err)
+	}
+
+	// to get the Port number, split the Host
+	hostport := strings.Split(url.Host, ":")
+	hostname := hostport[0]
+	// port := hostport[1]
+
+	if govalidator.IsURL(testurl) == false {
+		log.Println("Failed: Not a valid URL.")
+		h.Question.JobStatus = "Failed"
+		h.Question.JobMessage = "Not a valid URL"
+		return h
+	}
+
+	addrs, err := net.LookupHost(hostname)
+	if err != nil {
+		log.Println("DNS lookup failed: .", err)
+		h.Question.JobStatus = "Failed"
+		h.Question.JobMessage = "DNS lookup failed"
+		return h
+	}
+
+	// Get HTTP status.
+	req, err := http.NewRequest("GET", testurl, nil)
+	req.Header.Add("User-Agent", "AboutSecurity.nl Checker")
+
+	if err != nil {
+		log.Println("Failed: Can not connect.", err)
+		h.Question.JobStatus = "Failed"
+		h.Question.JobMessage = err.Error()
+		return h
+	}
+
+	resp, err := http.DefaultTransport.RoundTrip(req)
+	if err != nil {
+		log.Println("Failed connect.", err)
+		h.Question.JobStatus = "Failed"
+		h.Question.JobMessage = err.Error()
+		return h
+	}
+
+	h.Answer.HTTPStatus = resp.StatusCode
+	h.Answer.HTTPStatusText = resp.Status
+	h.Answer.HTTPProto = resp.Proto
+	h.Answer.IPAddress = addrs
+
+	timeout := time.Duration(2 * time.Second)
+	client := http.Client{
+		Timeout: timeout,
+	}
+	resp2, err := client.Get(testurl)
+	if err != nil {
+		h.Question.JobStatus = "Failed"
+		h.Question.JobMessage = err.Error()
+		return h
+	}
+	h.Answer.HTTPRedirect = resp2.Request.URL.String()
+
+	h.Answer.Connection = resp.Header.Get("Connection")
+
+	h.Question.JobStatus = "OK"
+	h.Question.JobMessage = "Job done!"
+
+	return h
+}
